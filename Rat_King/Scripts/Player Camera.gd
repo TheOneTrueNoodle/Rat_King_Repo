c52838505959a88610365c8a4b9704822b22f3e7
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2024-08-02 01:14:31 +0100

    New smoother camera

---
+++ Rat_King/Scripts/Player Camera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+@export_category("Follow Character")
+@export var player : CharacterBody2D
+
+@export_category("Camera Smoothing")
+@export var smoothing_enabled : bool
+@export_range(1, 10) var smoothing_distance : int = 8
+
+func _physics_process(delta):
+	if player != null: 
+		var camera_position : Vector2
+		
+		if smoothing_enabled:
+			var weight: float = float(11 - smoothing_distance) / 100
+			camera_position = lerp(global_position, player.global_position, weight)
+		else:
+			camera_position = player.global_position
+		
+		global_position = camera_position.floor()
