923c8eaebefb5c303f79dd85aadd8a2b9f4aaa84
Ryan O'Connor <72693746+TheOneTrueNoodle@users.noreply.github.com>
2024-08-05 17:42:25 +0100

    Developing Resource Display

--- Rat_King/Scenes/Base Building/Base Builder UI.gd
+++ Rat_King/Scenes/Base Building/Base Builder UI.gd
@@ -13,6 +13,23 @@ extends CanvasLayer
 @onready var buildingUpgradeButtonContainer = $"Construction Menu/Panel/ScrollContainer/VBoxContainer"
 var spawnedButtons = {}
 
+# Resource Display Variables
+@onready var resourceBuilding = $"../Resource Building"
+
+@onready var woodText = $"Resources Display/Wood/RichTextLabel"
+@onready var stoneText = $"Resources Display/Stone/RichTextLabel"
+@onready var metalText = $"Resources Display/Metal/RichTextLabel"
+@onready var medicineText = $"Resources Display/Medicine/RichTextLabel"
+@onready var foodText = $"Resources Display/Food/RichTextLabel"
+@onready var teethText = $"Resources Display/Teeth/RichTextLabel"
+
+func updateResourceDisplay():
+	woodText.text = str(resourceBuilding.resources["Wood"]) + " / " + str(resourceBuilding.baseMaxResource * resourceBuilding.buildingData.currentLevel)
+	stoneText.text = str(resourceBuilding.resources["Stone"]) + " / " + str(resourceBuilding.baseMaxResource * resourceBuilding.buildingData.currentLevel)
+	metalText.text = str(resourceBuilding.resources["Metal"]) + " / " + str(resourceBuilding.baseMaxResource * resourceBuilding.buildingData.currentLevel)
+	medicineText.text = str(resourceBuilding.resources["Medicine"]) + " / " + str(resourceBuilding.baseMaxResource * resourceBuilding.buildingData.currentLevel)
+	foodText.text = str(resourceBuilding.resources["Food"]) + " / " + str(resourceBuilding.baseMaxResource * resourceBuilding.buildingData.currentLevel)
+
 func openConstructionMenu():
 	openConstructionButton.visible = false
 	constructionMenu.visible = true
@@ -35,8 +52,8 @@ func openConstructionMenu():
 		newButton.pressed.connect(constructionBuilding.upgradeBuilding.bind(building))
 		newButton.pressed.connect(self.openConstructionMenu)
 		
-		if building.currentLevel >= constructionBuilding.buildingData.currentLevel and building != constructionBuilding.buildingData:
-			newButton.disabled = true
+		#Check if upgrade is affordable and can build it
+		newButton.disabled = CheckUpgradeCost(building)
 		
 		spawnedButtons[spawnedButtons.size()] = newButton
 
@@ -44,3 +61,19 @@ func closeConstructionMenu():
 	openConstructionButton.visible = true
 	constructionMenu.visible = false
 
+func CheckUpgradeCost(building: Building):
+	if building.currentLevel >= constructionBuilding.buildingData.currentLevel and building != constructionBuilding.buildingData:
+		return true
+	
+	#Check each resource type for cost
+	if resourceBuilding.resources["Wood"] < (building.baseWoodCost * (building.currentLevel + 1)):
+		return true
+	if resourceBuilding.resources["Stone"] < (building.baseStoneCost * (building.currentLevel + 1)):
+		return true
+	if resourceBuilding.resources["Metal"] < (building.baseMetalCost * (building.currentLevel + 1)):
+		return true
+	if resourceBuilding.resources["Medicine"] < (building.baseMedicineCost * (building.currentLevel + 1)):
+		return true
+	if resourceBuilding.resources["Food"] < (building.baseFoodCost * (building.currentLevel + 1)):
+		return true
+	return false
--- Rat_King/Scripts/Building Handler.gd
+++ Rat_King/Scripts/Building Handler.gd
@@ -6,5 +6,13 @@ class_name Building
 @export var maxLevel: int = 20
 @export var currentLevel: int = 0
 
+#Resource costs to upgrade!
+@export_group("Building Costs")
+@export var baseWoodCost: int = 0
+@export var baseStoneCost: int = 0
+@export var baseMetalCost: int = 0
+@export var baseMedicineCost: int = 0
+@export var baseFoodCost: int = 0
+
 func levelUp():
 	currentLevel += 1
